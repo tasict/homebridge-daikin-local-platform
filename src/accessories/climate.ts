be919a47e8882da5a499eac6a3c793699df1fdc0
Xiao Xiao <xx2004xiamen@gmail.com>
2023-12-18 15:46:46 +0900

    Compatibility support for humidifier mode

--- src/accessories/climate.ts
+++ src/accessories/climate.ts
@@ -8,6 +8,7 @@ import {
   CLIMATE_MODE_DEHUMIDIFY,
   CLIMATE_MODE_FAN,
   CLIMATE_MODE_HEATING,
+  CLIMATE_MODE_HUMIDIFY,
   CLIMATE_FAN_SPEED_AUTO,
   CLIMATE_FAN_SPEED_SLIENT,
   CLIMATE_FAN_SPEED_1,
@@ -303,6 +304,17 @@ export default class ClimateAccessory {
 
             // Dry (Dehumidifier)
             case CLIMATE_MODE_DEHUMIDIFY:
+              this.services['Climate'].getCharacteristic(this.platform.Characteristic.CurrentHeaterCoolerState)
+                .updateValue(this.platform.Characteristic.CurrentHeaterCoolerState.IDLE);
+                this.services['Climate'].updateCharacteristic(
+                this.platform.Characteristic.TargetHeaterCoolerState,
+
+                this.platform.Characteristic.TargetHeaterCoolerState.AUTO,
+              );
+              break;
+
+            // Humidifier
+            case CLIMATE_MODE_HUMIDIFY:
               this.services['Climate'].getCharacteristic(this.platform.Characteristic.CurrentHeaterCoolerState)
                 .updateValue(this.platform.Characteristic.CurrentHeaterCoolerState.IDLE);
                 this.services['Climate'].updateCharacteristic(
